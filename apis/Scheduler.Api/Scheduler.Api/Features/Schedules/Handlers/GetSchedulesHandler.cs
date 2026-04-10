7d56849b5426963d30e52a20baab7cdeb812d7c2
Andie Andrews <aandrews@paylocity.com>
2026-04-10 11:14:14 -0500

    fixing open api spec

--- apis/Scheduler.Api/Scheduler.Api/Features/Schedules/Handlers/GetSchedulesHandler.cs
+++ apis/Scheduler.Api/Scheduler.Api/Features/Schedules/Handlers/GetSchedulesHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Scheduler.Api.Features.Employee.Queries;
 using Scheduler.Api.Infrastructure.Data;
 using Scheduler.Api.Infrastructure.Domain.Models;
 
@@ -24,5 +25,27 @@ public class GetSchedulesHandler
         ";
 
     return await connection.QueryAsync<Schedule>(sql);
+  }
+
+  public async Task<IEnumerable<Schedule>> Handle(string query)
+  {
+    using var connection = _db.CreateConnection();
+
+    var sql = @"
+SELECT Id, Name
+FROM Schedules
+WHERE Name LIKE @query
+ORDER BY Name 
+";
+
+    var parameters = new
+    {
+      Query = $"%{query.Trim()}%"
+    };
+
+
+    return await connection.QueryAsync<Schedule>(
+      sql,
+      parameters);
   }
 }
