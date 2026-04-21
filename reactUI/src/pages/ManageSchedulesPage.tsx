83331f6a2614123db95df0076a9f519054f4c794
Ariel Wilson OBrien <awilsonobrien@paylocity.com>
2026-04-21 15:31:59 -0500

    Redo manage schedules page

--- reactUI/src/pages/ManageSchedulesPage.tsx
+++ reactUI/src/pages/ManageSchedulesPage.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useMemo, useState } from 'react'
-import { Button, Card, Group, Select, Stack, Table, Text, TextInput, Title } from '@mantine/core'
+import { Button, Card, Grid, Group, Select, Stack, Text, TextInput, Title } from '@mantine/core'
 import { api } from '../api/client'
 import { PageWithAssistant } from '../components/PageWithAssistant'
+import { ScheduleCalendar } from '../components/ScheduleCalendar'
 import { useAuth } from '../context/AuthContext'
 import type { Employee, Schedule, Shift } from '../types'
 import { addDays, formatWeekLabel, startOfWeekSunday, toIsoDate } from '../utils/date'
@@ -111,28 +112,34 @@ export function ManageSchedulesPage() {
         </Card>
 
         <Card withBorder p='xs' style={{ height: 'calc(100vh - 330px)', minHeight: 260, overflowY: 'auto' }}>
-          <Table striped highlightOnHover withTableBorder withColumnBorders>
-            <Table.Thead>
-              <Table.Tr>
-                <Table.Th>Employee</Table.Th>
-                <Table.Th>Shifts</Table.Th>
-                <Table.Th>Total Hours</Table.Th>
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-              {employees.map((employee) => {
-                const employeeShifts = shifts.filter((s) => s.employeeId === employee.id)
-                const totalHours = employeeShifts.reduce((acc, s) => acc + s.durationHours, 0)
-                return (
-                  <Table.Tr key={employee.id}>
-                    <Table.Td>{employee.firstName} {employee.lastName}</Table.Td>
-                    <Table.Td>{employeeShifts.length}</Table.Td>
-                    <Table.Td>{totalHours}</Table.Td>
-                  </Table.Tr>
-                )
-              })}
-            </Table.Tbody>
-          </Table>
+          <Stack gap='md'>
+            {employees.map((employee) => {
+              const employeeShifts = shifts.filter((s) => s.employeeId === employee.id)
+              const totalHours = employeeShifts.reduce((acc, s) => acc + s.durationHours, 0)
+              return (
+                <Grid key={employee.id} gutter='sm' align='stretch'>
+                  <Grid.Col span={3}>
+                    <Card withBorder p='sm' h='100%'>
+                      <Stack gap={4}>
+                        <Text fw={700}>
+                          {employee.firstName} {employee.lastName}
+                        </Text>
+                        <Text size='xs' c='dimmed'>
+                          Shifts: {employeeShifts.length}
+                        </Text>
+                        <Text size='xs' c='dimmed'>
+                          Total hours: {totalHours}
+                        </Text>
+                      </Stack>
+                    </Card>
+                  </Grid.Col>
+                  <Grid.Col span={9}>
+                    <ScheduleCalendar weekStart={weekStart} shifts={employeeShifts} />
+                  </Grid.Col>
+                </Grid>
+              )
+            })}
+          </Stack>
         </Card>
       </Stack>
     </PageWithAssistant>
